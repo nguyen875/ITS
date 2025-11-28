f326879772d376e7025559e505f96b444b52122d
Lmao1232j <vinhtran23042004@gmail.com>
2025-11-28 15:54:30 +0700

    Finish user management UI

--- Front End/src/App.jsx
+++ Front End/src/App.jsx
@@ -8,6 +8,7 @@ import MyCourses from "./pages/MyCourses";
 import { AuthProvider } from "./context/AuthContext";
 import CoursesPage from "./pages/CoursePage";
 import CoursesDetailPage from "./pages/CourseRegisteredPage";
+import AdminLayout from "./components/AdminMenu";
 
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="/my-courses" element={<MyCourses />} />
           <Route path="/course/:id" element={<CoursesPage />} />
           <Route path="/my-course/:id" element={<CoursesDetailPage />} />
+          <Route path="/admin-dashboard" element={<AdminLayout />} />
         </Routes>
       </Router>
     </AuthProvider>
